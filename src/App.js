f2ccc614d111ed9bae2833d6d794ff6d43cf8fbb
thomasravache <thomasravache31@gmail.com>
2021-11-24 00:10:48 -0300

    Atualizacao do app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Tabbar from './components/Tabbar';
+import Home from './components/Home';
+import About from './components/About';
 import useNavigation from './hooks/useNavigation';
 import appStyles from './appStyles';
+import Skills from './components/Skills';
 
 function App() {
   const { currentRoute, setCurrentRoute } = useNavigation();
   return (
     <div className={appStyles.container}>
       <Navbar currentRoute={ currentRoute } setCurrentRoute={ setCurrentRoute }/>
+      <Home />
+      <About />
+      <Skills />
       <Tabbar currentRoute={ currentRoute } setCurrentRoute={ setCurrentRoute }/>
     </div>
   );
--- src/appStyles.js
+++ src/appStyles.js
@@ -1,5 +1,5 @@
 const appStyles = {
-  container: 'bg-gray-200 dark:bg-black h-screen dark:bg-opacity-30'
+  container: 'bg-gray-200 dark:bg-black h-full dark:bg-opacity-30'
 };
 
 export default appStyles;
