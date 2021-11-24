1ea8a378ab02aa8cfa30dc99583c2a5ba0f1e2b3
thomasravache <thomasravache31@gmail.com>
2021-11-24 14:49:41 -0300

    atualizando formulario de contato

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,29 +26,29 @@ const Contact = () => {
           <form onSubmit={ sendEmail }>
             <div className="md:flex items-center mt-12">
               <div className="w-full flex flex-col">
-                  <label className="font-semibold leading-none">Nome</label>
-                  <input type="text" className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" name="name" />
+                  <label className="font-semibold leading-none">E-mail</label>
+                  <input placeholder="exemplo@email.com" type="email" required className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" name="email" />
               </div>
               {/* <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
                   <label className="font-semibold leading-none">Phone</label>
                   <input type="email" className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
               </div> */}
             </div>
-            <div className="md:flex items-center mt-8">
+            {/* <div className="md:flex items-center mt-8">
               <div className="w-full flex flex-col">
                   <label className="font-semibold leading-none">Subject</label>
                   <input type="text" className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" name="subject"/>
               </div>
-            </div>
+            </div> */}
             <div>
               <div className="w-full flex flex-col mt-8">
-                  <label className="font-semibold leading-none">Message</label>
-                  <textarea type="text" className="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" name="message"></textarea>
+                  <label className="font-semibold leading-none">Mensagem</label>
+                  <textarea placeholder="Digite sua mensagem aqui..." type="text" className="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" name="message" required></textarea>
               </div>
             </div>
             <div className="flex items-center justify-center w-full">
               <button className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
-                  Send message
+                  Enviar mensagem
               </button>
             </div>
           </form>
