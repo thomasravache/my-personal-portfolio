a1b74f035a0f404eeceea1fabf3367d0c4e2b193
thomasravache <thomasravache31@gmail.com>
2021-11-24 00:13:18 -0300

    Modificando alteracoes de estilo

--- src/components/tailwindStyles/navbarStyles.js
+++ src/components/tailwindStyles/navbarStyles.js
@@ -1,5 +1,5 @@
 const navbarStyles = {
-  navbar: 'hidden md:flex flex-row items-center justify-between px-8 h-20 rounded-b-3xl bg-white dark:bg-black',
+  navbar: 'sticky z-50 top-0 hidden md:flex fixed w-full flex-row items-center justify-between px-8 h-20 rounded-b-3xl bg-white dark:bg-black',
   logo: 'text-4xl text-gray-800 dark:text-white -mb-1',
   navItems: 'flex flex-row self-end h-full',
   navItem: 'pt-8 w-20 text-gray-400 dark:text-white hover:text-gray-700 dark:hover:text-blue-300 cursor-pointer font-medium tracking-wide text-sm flex items-start justify-center mx-4',
